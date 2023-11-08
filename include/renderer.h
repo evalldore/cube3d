ce43467fd96e4f76c8a8bbac6cbac9296697c14f
Eliott Vallee dore <evallee-@c1r5p3.42quebec.com>
2023-11-08 18:11:07 -0500

    norm change

--- include/renderer.h
+++ include/renderer.h
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   renderer.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 15:22:21 by evallee-          #+#    #+#             */
+/*   Updated: 2023/11/08 15:23:44 by evallee-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef RENDERER_H
+# include <stdint.h>
 # define RENDERER_H
 # define WIDTH 800
 # define HEIGHT 600
@@ -6,6 +19,6 @@
 void		r_init(void	*param);
 void		r_clear(uint32_t color);
 uint32_t	r_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
-void		*r_get_buffer();
+void		*r_get_buffer(void);
 
 #endif
--- src/ray.c
+++ src/ray.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:01:49 by niceguy           #+#    #+#             */
-/*   Updated: 2023/08/25 19:17:24 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/11/08 18:02:28 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	ray_init(t_ray *r, t_uvec *check, t_fvec *step, t_fvec *length)
 {
 	check->x = (uint32_t)r->pos.x;
 	check->y = (uint32_t)r->pos.y;
-	step->x = INFINITY;
+	step->x = INFINITY; // INFINITY NOT VALID
 	step->y = INFINITY;
 	if (r->dir.x != 0.0f)
 		step->x = fabs(1.0f / r->dir.x);
