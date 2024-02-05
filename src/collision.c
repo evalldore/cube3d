df4d9fa257005a070c792febc953e76b231eca71
evalldore <evalldore@gmail.com>
2024-02-04 23:28:29 -0500

    simplified code and world struct

--- src/collision.c
+++ src/collision.c
@@ -6,7 +6,7 @@
 /*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:00:04 by evallee-          #+#    #+#             */
-/*   Updated: 2024/02/04 23:26:09 by niceguy          ###   ########.fr       */
+/*   Updated: 2024/02/04 23:27:52 by niceguy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@ t_ivec	collision_check(t_fvec pos, t_fvec dir)
 	t_uvec	check;
 	t_ivec	res;
 
+	res = (t_ivec){0, 0};
 	check.x = (uint32_t)(pos.x + dir.x * 0.5f);
 	check.y = (uint32_t)pos.y;
-	res = (t_ivec){0, 0};
-	if (world_is_wall(check))
-		res.x = 1;
+	res.x = world_is_wall(check);
 	check.x = (uint32_t)pos.x;
 	check.y = (uint32_t)(pos.y + dir.y * 0.5f);
-	if (world_is_wall(check))
-		res.y = 1;
+	res.y = world_is_wall(check);
 	return (res);
 }
